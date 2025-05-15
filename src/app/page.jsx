d53449aadc5e4dbe5bdb7b4e55edc97c3a838f63
Vitor Sampaio <vitorsampaio.tds2.senai@gmail.com>
2025-05-15 08:32:19 -0300

    reafctor: inicial configs

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Games Retrô</h1>
+      <p>Recordes de Jogos Retrô</p>
+    </main>
+      
+  );
+}
