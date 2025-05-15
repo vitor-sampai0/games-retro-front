5c00d17852117f53e876742ec1a5c43173167ea0
Vitor Sampaio <vitorsampaio.tds2.senai@gmail.com>
2025-05-15 08:49:28 -0300

    feat: create games pages

---
+++ src/app/games/page.jsx
@@ -0,0 +1,9 @@
+import styles from './games.module.css';
+
+export default function Games() {
+    return (
+        <div className={styles.main}>
+            <h1>Página de Games</h1>
+        </div>
+    );
+}
